aba1a2ce8d19ba24e97c41795bfa25246cdee205
nthumu <44855085+nthumu@users.noreply.github.com>
2022-02-11 12:00:15 +0530

    Created Method update_scoreboard to update the score when turtle crosses the line

--- scoreboard.py
+++ scoreboard.py
@@ -10,7 +10,12 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.penup()
         self.goto(-280, 280)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
         self.write(f"Level: {self.level}", align="left", font=FONT)
 
     def increase_level(self):
         self.level += 1
+        self.update_scoreboard()
